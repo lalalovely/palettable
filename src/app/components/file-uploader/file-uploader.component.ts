4a2f5d1794a640c35b776c8f6cd5a99c49ba4df6
lovelygracearsolon <lovelygracearsolon@gmail.com>
2022-10-27 15:05:48 +0800

    Add error handler for too large photos

--- src/app/components/file-uploader/file-uploader.component.ts
+++ src/app/components/file-uploader/file-uploader.component.ts
@@ -6,26 +6,43 @@ import { Component, EventEmitter, OnInit, Output } from "@angular/core";
   styleUrls: ["./file-uploader.component.scss"],
 })
 export class FileUploaderComponent implements OnInit {
-  fileName: string = "";
-  @Output() imageSource = new EventEmitter<any>();
+  @Output() imageSource = new EventEmitter<string>();
+
+  private MAX_FILE_SIZE_BYTES = 5000000;
+
+  hasError: boolean = false;
+  error: string = "";
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onFileDropped($event: Array<any>) {
-    this.fileName = URL.createObjectURL($event[0]);
-    this.imageSource.emit(this.fileName);
+    const fileName = URL.createObjectURL($event[0]);
+    this.imageSource.emit(fileName);
   }
 
   onFileChange($event: Event) {
     this.handleFileUpload($event);
-    this.imageSource.emit(this.fileName);
   }
 
   handleFileUpload(e: Event) {
     const target = e.target as HTMLInputElement;
     const files = target.files as FileList;
-    this.fileName = URL.createObjectURL(files[0]);
+
+    const newFile = files[0];
+
+    if (newFile.size > this.MAX_FILE_SIZE_BYTES) {
+      this.hasError = true;
+      this.error = "The photo you uploaded is too large!";
+    } else {
+      const fileName = URL.createObjectURL(files[0]);
+      this.imageSource.emit(fileName);
+    }
+  }
+
+  tryAgain() {
+    this.hasError = false;
+    this.error = "";
   }
 }
